fd55589709407587edc0ae9af62ec9d2a31c8457
MarshallRifqi <marshallrifqi2@gmail.com>
2024-05-01 10:31:08 +0700

    menambahkan style dan input time

--- src/App.vue
+++ src/App.vue
@@ -1,86 +1,154 @@
-<script setup>
+<!-- <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue'
+</script> -->
+
+<script setup>
+import { ref, onMounted, computed, watch } from 'vue'
+
+const todos = ref([])
+const name = ref('')
+
+const input_content = ref('')
+const input_time = ref(null)
+
+const todos_asc = computed(() => todos.value.sort((a,b) =>{
+	return a.createdAt - b.createdAt
+}))
+
+watch(name, (newVal) => {
+	localStorage.setItem('name', newVal)
+})
+
+watch(todos, (newVal) => {
+	localStorage.setItem('todos', JSON.stringify(newVal))
+}, {
+	deep: true
+})
+
+const addTodo = () => {
+	if (input_content.value.trim() === '') {
+		return
+	}
+
+	todos.value.push({
+		content: input_content.value,
+		time: input_time.value,
+		done: false,
+		editable: true,
+		createdAt: new Date().getTime()
+	})
+}
+
+const removeTodo = (todo) => {
+	todos.value = todos.value.filter((t) => t !== todo)
+}
+
+onMounted(() => {
+	name.value = localStorage.getItem('name') || ''
+	todos.value = JSON.parse(localStorage.getItem('todos')) || []
+})
+
+const editTodo = (todo) => {
+	const newTitle = prompt('Edit Todo Title:', todo.content)
+	const newTime = prompt('Edit Todo Time:', todo.time)
+	if (newTitle !== null) {
+		todo.content = newTitle.trim()
+		todo.time = newTime.trim()
+	}
+}
+
+// clock
+
+//end of clock
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+	<main class="app">
+		
+		<section class="greeting">
+			<h2 class="title">
+				Halo,<input type="text" id="name" placeholder="Nama" v-model="name">
+			</h2>
+		</section>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+		<section class="create-todo">
+			<div class="title-wrapper">
+				<h3 id="todo-title">Create a To-do</h3>
+			</div>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/todo">Todo</RouterLink>
-      </nav>
-    </div>
-  </header>
+			<form id="new-todo-form" @submit.prevent="addTodo">
+				<h4>What's on your todo list?</h4>
+				<input 
+					type="text" 
+					name="content" 
+					id="content" 
+					placeholder="e.g. make a Homework"
+					v-model="input_content" />
+					<input type="time" class="time-pickable" v-model="input_time">
+				
+				<!-- <h4>Pick a category</h4>
+				<div class="options">
 
-  <RouterView />
+					<label>
+						<input 
+							type="radio" 
+							name="category" 
+							id="category1" 
+							value="business"
+							v-model="input_category" />
+						<span class="bubble business"></span>
+						<div>Business</div>
+					</label>
+
+					<label>
+						<input 
+							type="radio" 
+							name="category" 
+							id="category2" 
+							value="personal"
+							v-model="input_category" />
+						<span class="bubble personal"></span>
+						<div>Personal</div>
+					</label>
+
+				</div> -->
+
+				<input type="submit" value="Add todo" />
+			</form>
+		</section>
+
+		<section class="todo-list">
+			<div class="title-wrapper">
+				<h3>List of Your To-Do</h3>
+			</div>
+			<div class="list" id="todo-list">
+
+				<div v-for="todo in todos_asc" :class="`todo-item ${todo.done && 'done'}`">
+					<label>
+						<input type="checkbox" v-model="todo.done" />
+						<span :class="`bubble ${
+							todo.category == 'business' 
+								? 'business' 
+								: 'personal'
+						}`"></span>
+					</label>
+
+					<div class="todo-content">
+						<input type="text" v-model="todo.content" readonly />
+						<input type="text" v-model="todo.time" readonly style="color: #888;"/>
+					</div>
+
+					<div class="actions">
+						<button class="update" @click="editTodo(todo)">Edit</button>
+						<button class="delete" @click="removeTodo(todo)">Delete</button>
+					</div>
+				</div>
+
+			</div>
+		</section>
+
+	</main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
